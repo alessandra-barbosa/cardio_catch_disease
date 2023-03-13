3e3fe784f264e0aa17f651b4a3da43dbb540fa0e
Alessandra Barbosa <a.deabarbosa@gmail.com>
2023-03-13 12:10:00 +0100

    delet study

--- study/pa001_cardio_catch_diseases-pa001_diego_mayer/web/app/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Patients
-
-class PatientForm(forms.ModelForm):
-    
-    class Meta:
-        model = Patients
-        fields = ('id', 'name', 'age', 'gender', 'height', 'weight', 'hight_pressure', 'low_pressure', 
-                  'cholesterol', 'glucose', 'smoker', 'alcohol', 'active', 'cardio')
--- study/pa001_cardio_catch_diseases-pa001_diego_mayer/web/web/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from app.views import form_post, prediction
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', form_post, name='form'),
-    path('prediction/', prediction, name='prediction'),
-]
